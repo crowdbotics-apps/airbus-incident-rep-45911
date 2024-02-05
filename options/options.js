0d12458e1b8112d7fb8a750eada1fff2c719347e
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:51:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "airbus-incident-rep-45911",
+  url: "https://airbus-incident-rep-45911.botics.co",
+  api: "https://airbus-incident-rep-45911.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
